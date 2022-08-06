fa0fcf114caf2e96d9864944b969b1a9fea93bb7
aswafford358 <aswafford@southern.edu>
2022-08-05 22:21:31 -0400

    Initial commit.

---
+++ client.go
@@ -0,0 +1,67 @@
+// client.go
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"github.com/gorilla/websocket"
+)
+
+var done chan interface{}
+var interrupt chan os.Signal
+
+var reader = bufio.NewReader(os.Stdin)
+
+func readKeys(input chan rune, interrupt chan os.Signal) {
+	for {
+		select {
+		case <-interrupt:
+			break
+		default:
+			char, _, err := reader.ReadRune()
+			if err != nil {
+				log.Fatal(err)
+			}
+			input <- char
+		}
+	}
+}
+
+func receiveHandler(connection *websocket.Conn) {
+	defer close(done)
+	for {
+		_, msg, err := connection.ReadMessage()
+		if err != nil {
+			log.Println("Error in receive:", err)
+			return
+		}
+		log.Printf("Received: %s\n", msg)
+	}
+}
+
+func main() {
+
+	// host := os.Getenv("ws_host_to_test")
+
+	done = make(chan interface{}) // Channel to indicate that the receiverHandler is done
+
+	socketUrl := "ws://localhost:8080" + "/ws"
+	conn, _, err := websocket.DefaultDialer.Dial(socketUrl, nil)
+	if err != nil {
+		log.Fatal("Error connecting to Websocket Server:", err)
+	}
+	defer conn.Close()
+	go receiveHandler(conn)
+
+	// Our main loop for the client
+	// We send our relevant packets here
+	for {
+		inputLine, _, _ := reader.ReadLine()
+		err := conn.WriteMessage(websocket.TextMessage, []byte(string(inputLine)))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module alex-swafford/test-ws-client
+
+go 1.17
+
+require github.com/gorilla/websocket v1.5.0 // indirect
